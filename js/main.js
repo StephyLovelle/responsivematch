3cc6d3caaa89b51d1f97ac62ff0f38b1292f5df9
StephyFCB <stephylovelle@gmail.com>
2015-07-03 22:44:06 -0400

    responsive touts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function() {
-  // Handler for .ready() called.
+document.getElementsByTagName("li")[0].addEventListener("click",function(){
+	if(!this.parentNode.getAttribute("class")){
+		this.parentNode.setAttribute("class","slideDown");
+	}
+	else{
+		this.parentNode.removeAttribute("class");
+	}
 });
